ccc73ad6c8dfd5aabf325dcc93b7c200b6f5a31a
qinrn <3165511966@qq.com>
2020-07-01 13:59:57 +0800

    This is test

--- cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -17,6 +17,7 @@ public class Payment implements Serializable {
     public static void main(String[] args) {
         System.out.println("Hello ~this is Liu");
         System.out.println("I am liubaolai");
+        System.out.println("i an qinruo nan");
     }
 }
 
