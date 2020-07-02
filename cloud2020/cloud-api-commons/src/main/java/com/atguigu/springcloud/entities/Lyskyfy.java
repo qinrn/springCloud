ce20a511a9522718d19a02f18459ef18d58a7753
lyskyfy <lyskyfy@gmial.com>
2020-07-02 10:17:51 +0800

    This is new added

---
+++ cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Lyskyfy.java
@@ -0,0 +1,9 @@
+package com.atguigu.springcloud.entities;
+
+public class Lyskyfy {
+
+    public static void main(String[] args) {
+        System.out.println("This is new add file 2020-0702");
+    }
+
+}
