edff359cd43283289f419918a0702a1905d9c5c7
qinrn <3165511966@qq.com>
2020-07-02 11:26:55 +0800

    Test0702 ~ 1123

--- cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -15,9 +15,6 @@ public class Payment implements Serializable {
     private String serial;
 
     public static void main(String[] args) {
-
-        System.out.println("i delete this");
-        System.out.println("new file");
         System.out.println("Test 1123");
     }
 }
