832046b1e71498df2505fd02101bb62e6192d3c1
qinrn <3165511966@qq.com>
2020-07-02 11:32:11 +0800

    Test merge

--- cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ cloud2020/cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -15,7 +15,12 @@ public class Payment implements Serializable {
     private String serial;
 
     public static void main(String[] args) {
+
+        System.out.println("i delete this");
+        System.out.println("new file");
         System.out.println("Test 1123");
+
+        System.out.println("Test merge");
     }
 }
 
